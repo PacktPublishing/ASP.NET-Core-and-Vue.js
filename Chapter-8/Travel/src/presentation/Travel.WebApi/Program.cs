c6fe4bb65d9a3b49566b5d29c04705b5af42578c
devlin <73556235+mandaloriandeveloper@users.noreply.github.com>
2020-11-16 21:34:24 +0100

    updated

--- Chapter-8/Travel/src/presentation/Travel.WebApi/Program.cs
+++ Chapter-8/Travel/src/presentation/Travel.WebApi/Program.cs
@@ -1,12 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Events;
 using Serilog.Exceptions;
@@ -29,11 +24,13 @@ namespace Travel.WebApi
                 .Enrich.WithProperty("Assembly", $"{name.Version}")
                 .WriteTo.SQLite(
                         Environment.CurrentDirectory + @"/Logs/log.db",
+                        // Environment.CurrentDirectory + @"\Logs\log.db",
                         restrictedToMinimumLevel: LogEventLevel.Information,
                         storeTimestampInUtc: true)
                 .WriteTo.File(
                         new CompactJsonFormatter(),
                         Environment.CurrentDirectory + @"/Logs/log.json",
+                        // Environment.CurrentDirectory + @"\Logs\log.json",
                         rollingInterval: RollingInterval.Day,
                         restrictedToMinimumLevel: LogEventLevel.Information)
                 .WriteTo.Console()
