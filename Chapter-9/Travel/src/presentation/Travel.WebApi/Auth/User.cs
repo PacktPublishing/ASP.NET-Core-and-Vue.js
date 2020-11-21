f8350f69c71a3e095ce568505b0f9f220aba1dc0
devlin <73556235+mandaloriandeveloper@users.noreply.github.com>
2020-11-21 10:12:10 +0100

    updated

---
+++ Chapter-9/Travel/src/presentation/Travel.WebApi/Auth/User.cs
@@ -0,0 +1,15 @@
+using System.Text.Json.Serialization;
+
+namespace WebApi.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+
+        [JsonIgnore]
+        public string Password { get; set; }
+    }
+}
