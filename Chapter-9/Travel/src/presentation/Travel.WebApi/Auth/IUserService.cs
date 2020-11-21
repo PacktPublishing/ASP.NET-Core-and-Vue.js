84bb0cdf805caf9b09a015934d35acc4a63372a5
devlin <73556235+mandaloriandeveloper@users.noreply.github.com>
2020-11-21 14:26:35 +0100

    updated

---
+++ Chapter-9/Travel/src/presentation/Travel.WebApi/Auth/IUserService.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace Travel.WebApi.Auth
+{
+    public interface IUserService
+    {
+        AuthenticateResponse Authenticate(AuthenticateRequest model);
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+    }
+}
