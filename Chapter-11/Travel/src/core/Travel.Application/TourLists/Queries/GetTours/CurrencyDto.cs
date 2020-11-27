d9bcf800696503dc131c883d10098602399335bb
Devlin Duldulao <webmasterdevlin@gmail.com>
2020-11-27 23:27:16 +0100

    updated

---
+++ Chapter-11/Travel/src/core/Travel.Application/TourLists/Queries/GetTours/CurrencyDto.cs
@@ -0,0 +1,8 @@
+namespace Travel.Application.TourLists.Queries.GetTours
+{
+    public class CurrencyDto
+    {
+        public int Value { get; set; }
+        public string Name { get; set; }
+    }
+}
