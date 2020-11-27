bf6a5f949a95bdfa735a4f6f4fd616cd3c0fecc3
Devlin Duldulao <webmasterdevlin@gmail.com>
2020-11-27 21:49:20 +0100

    updated

--- Chapter-11/Travel/src/core/Travel.Application/TourPackages/Queries/GetTourPackagesQuery.cs
+++ Chapter-11/Travel/src/core/Travel.Application/TourPackages/Queries/GetTourPackagesQuery.cs
@@ -1,36 +1,40 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Travel.Application.Common.Interfaces;
 using Travel.Application.Dtos.Tour;
 
 namespace Travel.Application.TourPackages.Queries
 {
-    public class GetTourIPackagesQuery : IRequest<TourPackageDto>
+    public class GetTourPackagesQuery : IRequest<List<TourPackageDto>>
     {
         public int ListId { get; set; }
     }
 
-    public class GetTourIPackagesQueryHandler : IRequestHandler<GetTourIPackagesQuery, TourPackageDto>
+    public class GetTourPackagesQueryHandler : IRequestHandler<GetTourPackagesQuery, List<TourPackageDto>>
     {
         private readonly IApplicationDbContext _context;
         private readonly IMapper _mapper;
 
-        public GetTourIPackagesQueryHandler(IApplicationDbContext context, IMapper mapper)
+        public GetTourPackagesQueryHandler(IApplicationDbContext context, IMapper mapper)
         {
             _context = context;
             _mapper = mapper;
         }
 
-        public  Task<TourPackageDto> Handle(GetTourIPackagesQuery request, CancellationToken cancellationToken)
+        public  Task<List<TourPackageDto>> Handle(GetTourPackagesQuery request, CancellationToken cancellationToken)
         {
             var tourPackages =  _context.TourPackages
                 .Where(t => t.ListId == request.ListId)
                 .OrderBy(t => t.Name)
-                .ProjectTo<TourPackageDto>(_mapper.ConfigurationProvider);
+                .ProjectTo<TourPackageDto>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+            
             return tourPackages;
         }
     }
