48c13a9cb10f965400f654e69f93ebf32ebcc934
Devlin Duldulao <webmasterdevlin@gmail.com>
2020-12-03 22:52:11 +0100

    updated

--- Chapter-15/Travel/src/presentation/Travel.WebApi/Controllers/v2/WeatherForecastController.cs
+++ Chapter-15/Travel/src/presentation/Travel.WebApi/Controllers/v2/WeatherForecastController.cs
@@ -28,16 +28,67 @@ namespace Travel.WebApi.Controllers.v2
         [HttpPost]
         public IEnumerable<WeatherForecast> Post(string city)
         {
-
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+
+            return Enumerable.Range(1, 14).Select(index =>
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)],
-                City = city
+                var celcius = rng.Next(-20, 55);
+
+                return new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = celcius,
+                    Summary = GetSummary(celcius),
+                    City = city
+                };
             })
             .ToArray();
+        }
+
+        private static string GetSummary(int temp)
+        {
+            if (temp > -20 && temp < -10)
+            {
+                return Summaries[0];
+            }
+            else if (temp > -9 && temp < 0)
+            {
+                return Summaries[1];
+            }
+            else if (temp > 1 && temp < 10)
+            {
+                return Summaries[2];
+            }
+            else if (temp > 11 && temp < 20)
+            {
+                return Summaries[3];
+            }
+            else if (temp > 1 && temp < 10)
+            {
+                return Summaries[4];
+            }
+            else if (temp > 11 && temp < 20)
+            {
+                return Summaries[5];
+            }
+            else if (temp > 21 && temp < 30)
+            {
+                return Summaries[6];
+            }
+            else if (temp > 31 && temp < 40)
+            {
+                return Summaries[7];
+            }
+            else if (temp > 41 && temp < 50)
+            {
+                return Summaries[8];
+            }
+            else if (temp > 51 && temp < 55)
+            {
+                return Summaries[9];
+            }
+
+            return "Not Advisable";
         }
     }
 }
