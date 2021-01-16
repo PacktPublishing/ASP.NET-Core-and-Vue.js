ca8e09fc059c3e2cbcd6d5a934d6e548afd85f49
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-01-16 21:51:14 +0100

    updated

---
+++ Chapter-16/Travel/src/presentation/vue-app/src/auth/views/ContinueAs.vue
@@ -0,0 +1,33 @@
+
+<template>
+  <div class="container" v-if="email">
+    <div class="text-h4 my-5">Do you like to continue as {{email}}? 🔒</div>
+    <v-btn  @click="onSubmit" color="primary" class="mr-4">Yes</v-btn>
+    <v-btn @click="handleLogOut()" outlined color="primary">No thanks, I'd like to log out</v-btn>
+  </div>
+  <div v-else class="text-h4 m-10">
+    Fancy meeting you here. <v-btn @click="handleLogOut()" outlined color="primary">Go to Home Page</v-btn>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+import { getUserEmailFromToken, logOut } from '@/auth/auth.service'
+export default {
+  name: 'ContinueAs',
+
+  data: () => ({
+    email: getUserEmailFromToken()
+  }),
+  
+  methods: {
+    ...mapActions("authModule", ["useLocalStorageTokenToSignIn"]),
+    handleLogOut() {
+      logOut();
+    },
+    onSubmit() {
+        this.$router.push({ path: "/admin-dashboard" });
+    }
+  }
+}
+</script>
