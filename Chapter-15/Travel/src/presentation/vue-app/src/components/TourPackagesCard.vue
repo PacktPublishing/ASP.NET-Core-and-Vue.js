4be88b7554cb27c05d2497f825a616ef12dc1eae
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-01-16 09:19:30 +0100

    updated

---
+++ Chapter-15/Travel/src/presentation/vue-app/src/components/TourPackagesCard.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-container>
+    <v-card width="500" max-width="600">
+      <v-toolbar color="pink" dark>
+        <v-toolbar-title>Packages</v-toolbar-title>
+        <v-spacer></v-spacer>
+      </v-toolbar>
+      <v-list two-line>
+        <v-list-item-group active-class="pink--text">
+          <div v-if="packages && packages.length > 0">
+            <template v-for="tourPackage in packages">
+              <v-list-item :key="tourPackage.id">
+                <template>
+                  <v-list-item-content>
+                    <v-list-item-title
+                      v-text="tourPackage.name"
+                    ></v-list-item-title>
+                    <v-list-item-subtitle
+                      class="text--primary"
+                      v-text="tourPackage.whatToExpect"
+                    ></v-list-item-subtitle>
+                    <div
+                      style="
+                        margin-top: 0.5rem;
+                        display: flex;
+                        flex-direction: row;
+                      "
+                    >
+                      <v-list-item-subtitle
+                        class="text--secondary"
+                        v-text="`Duration: ${tourPackage.duration}hrs`"
+                      ></v-list-item-subtitle>
+                      <v-list-item-subtitle
+                        class="text--secondary"
+                        v-text="
+                          `${
+                            tourPackage.instantConfirmation
+                              ? 'Instant Confirmation'
+                              : ''
+                          }`
+                        "
+                      ></v-list-item-subtitle>
+                    </div>
+                  </v-list-item-content>
+                  <v-list-item-action>
+                    <v-icon> mdi-delete-outline </v-icon>
+                  </v-list-item-action>
+                </template>
+              </v-list-item>
+            </template>
+            <v-divider />
+          </div>
+          <div v-else>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title
+                  v-text="'No package added yet 😢'"
+                ></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </div>
+        </v-list-item-group>
+      </v-list>
+    </v-card>
+  </v-container>
+</template>
+
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+export default {
+  name: "TourPackagesCard",
+
+  computed: {
+    ...mapGetters("tourModule", {
+      packages: "packagesOfSelectedCity",
+    }),
+  },
+
+  methods: {
+    ...mapActions("tourModule", ["removeTourPackageAction"]),
+  },
+};
+</script>
+
