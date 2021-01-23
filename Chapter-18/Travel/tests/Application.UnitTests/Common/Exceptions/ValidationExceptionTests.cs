e87179acc727d5ddd2bb32c9c87c786966fba44a
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-01-23 12:24:00 +0100

    updated

--- Chapter-18/Travel/tests/Application.UnitTests/Common/Exceptions/ValidationExceptionTests.cs
+++ Chapter-18/Travel/tests/Application.UnitTests/Common/Exceptions/ValidationExceptionTests.cs
@@ -1,4 +1,35 @@
-$HEADER$namespace $NAMESPACE$
+using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using FluentValidation.Results;
+using Travel.Application.Common.Exceptions;
+using Xunit;
+
+namespace Application.UnitTests.Common.Exceptions
 {
-  public class $CLASS$ {$END$}
+    public class ValidationExceptionTests
+    {
+
+        [Fact]
+        public void DefaultConstructorCreatesAnEmptyErrorDictionary()
+        {
+            var actual = new ValidationException().Errors;
+
+            actual.Keys.Should().BeEquivalentTo(Array.Empty<string>());
+        }
+
+        [Fact]
+        public void SingleValidationFailureCreatesASingleElementErrorDictionary()
+        {
+            var failures = new List<ValidationFailure>
+            {
+                new ("Game", "Game is required.")
+            };
+
+            var actual = new ValidationException(failures).Errors;
+
+            actual.Keys.Should().BeEquivalentTo("Game");
+            actual["Game"].Should().BeEquivalentTo("Game is required.");
+        }
+    }
 }
--- Chapter-18/Travel/tests/Application.UnitTests/Common/Mappings/MappingTests.cs
+++ Chapter-18/Travel/tests/Application.UnitTests/Common/Mappings/MappingTests.cs
@@ -1,7 +1,40 @@
+using System;
+using AutoMapper;
+using Travel.Application.Common.Mappings;
+using Travel.Application.Dtos.Tour;
+using Travel.Domain.Entities;
+using Xunit;
+
 namespace Application.UnitTests.Common.Mappings
 {
     public class MappingTests
     {
-        
+        private readonly IConfigurationProvider _configuration;
+        private readonly IMapper _mapper;
+
+        public MappingTests()
+        {
+            _configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+
+            _mapper = _configuration.CreateMapper();
+        }
+
+        [Fact]
+        public void ShouldHaveValidConfiguration()
+        {
+            _configuration.AssertConfigurationIsValid();
+        }
+
+        [Theory]
+        [InlineData(typeof(TourList), typeof(TourListDto))]
+        public void ShouldSupportMappingFromSourceToDestination(Type source, Type destination)
+        {
+            var instance = Activator.CreateInstance(source);
+
+            _mapper.Map(instance, source, destination);
+        }
     }
 }
