e4cfd67c9969132fcf13fea311adbce532649f7d
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-01-25 00:50:00 +0100

    updated

---
+++ Chapter-18/Travel/src/infrastructure/Travel.Data/Migrations/20210124234641_InitialCreate.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Travel.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TourLists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    About = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TourLists", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TourPackages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ListId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WhatToExpect = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MapLocation = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<float>(type: "real", nullable: false),
+                    Duration = table.Column<int>(type: "int", nullable: false),
+                    InstantConfirmation = table.Column<bool>(type: "bit", nullable: false),
+                    Currency = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TourPackages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TourPackages_TourLists_ListId",
+                        column: x => x.ListId,
+                        principalTable: "TourLists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TourPackages_ListId",
+                table: "TourPackages",
+                column: "ListId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TourPackages");
+
+            migrationBuilder.DropTable(
+                name: "TourLists");
+        }
+    }
+}
