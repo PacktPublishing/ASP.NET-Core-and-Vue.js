80666809f40c7731f7531d69737d9fd90bb454f2
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-02-07 23:12:39 +0100

    updated

--- Chapter-19/Travel/WebUI/ClientApp/src/views/Home.vue
+++ Chapter-19/Travel/WebUI/ClientApp/src/views/Home.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Edited 😜 : Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent } from "vue";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default defineComponent({
-  name: 'Home',
+  name: "Home",
   components: {
     HelloWorld,
   },
